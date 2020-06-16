83ca293772a01e08485e6bf1a8179d91e5aac1ec
James Hagans <jhagans@acm.org>
2020-06-16 00:17:07 -0400

    Managing the Heights of a Skip List

--- building-linked-lists-and-binary-trees/src/bin/skip_list.rs
+++ building-linked-lists-and-binary-trees/src/bin/skip_list.rs
@@ -1,10 +1,84 @@
 use std::cell::RefCell;
+use std::fmt;
+use std::fmt::{Debug, Write};
 use std::rc::Rc;
 
 type Rcc<T> = Rc<RefCell<T>>;
 
 pub fn rcc<T>(t: T) -> Rcc<T> {
     Rc::new(RefCell::new(t))
+}
+#[derive(Debug)]
+pub struct SkipList<T: PartialOrd>(Vec<SkipNode<T>>);
+
+impl<T: PartialOrd> SkipList<T> {
+    pub fn new() -> Self {
+        SkipList(Vec::new())
+    }
+
+    pub fn insert(&mut self, data: T) {
+        if self.0.len() == 0 {
+            self.0.push(SkipNode::new(data));
+            return;
+        }
+
+        // Our vec will have the lowest row, with the lowest number,
+        // we need to try and insert in the highest available row.
+        for i in (0..self.0.len()).rev() {
+            if data > *self.0[i].data.borrow() {
+                if let Some(ch) = self.0[i].insert(data) {
+                    self.loop_up(ch, i + 1);
+                    // loop up on 50/50 chance
+                }
+                return;
+            }
+        }
+        // if none of those succeeded, that means we have an
+        // element to replace the first
+        let mut nn = SkipNode::new(data);
+        // put our new element on the front of the row
+        std::mem::swap(&mut nn, &mut self.0[0]);
+        let res = rcc(nn);
+        self.0[0].right = Some(res.clone());
+        self.loop_up(res, 1);
+        // loop up on 50/50 chance
+    }
+
+    pub fn loop_up(&mut self, ch: Rcc<SkipNode<T>>, n: usize) {
+        if rand::random::<bool>() == true {
+            return;
+        }
+        let dt = ch.borrow().data.clone();
+        let mut nn = SkipNode {
+            right: None,
+            down: Some(ch),
+            data: dt,
+        };
+        if n >= self.0.len() {
+            self.0.push(nn);
+            return;
+        }
+
+        std::mem::swap(&mut nn, &mut self.0[n]);
+        let res = rcc(nn);
+        self.0[n].right = Some(res.clone());
+        self.loop_up(res, n + 1);
+    }
+}
+
+impl<T: Debug + PartialOrd> fmt::Display for SkipList<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if self.0.len() == 0 {
+            return write!(f, "Skiplist<Empty>");
+        }
+
+        for sn in &self.0 {
+            write!(f, "\n")?;
+            sn.print_row(f)?;
+        }
+
+        Ok(())
+    }
 }
 
 #[derive(Debug)]
@@ -64,9 +138,21 @@ impl<T: PartialOrd> SkipNode<T> {
     }
 }
 
+impl<T: Debug + PartialOrd> SkipNode<T> {
+    pub fn print_row<W: Write>(&self, w: &mut W) -> std::fmt::Result {
+        write!(w, "{:?}", self.data.borrow())?;
+        if let Some(ref r) = self.right {
+            write!(w, ",")?;
+            r.borrow().print_row(w)?;
+        }
+
+        Ok(())
+    }
+}
+
 fn main() {
     // cargo run --bin skip_list
-    let mut s = SkipNode::new(4);
+    let mut s = SkipList::new();
     s.insert(4);
     s.insert(6);
     s.insert(77);
@@ -74,5 +160,9 @@ fn main() {
     s.insert(23);
     s.insert(1);
 
-    println!("s: {:#?}", s);
+    // println!("s: {:?}", s);
+    // Display
+    println!("s: {}", s);
 }
+
+// add find, pop, remove methods
