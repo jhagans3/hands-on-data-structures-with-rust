5406e20ac210726ecece5c373c5e3e8a96032873
James Hagans <jhagans@acm.org>
2020-06-19 23:59:19 -0400

    Filling the Graph

---
+++ model-real-situations-as-graphs-of-connected-nodes/src/bin/graph.rs
@@ -0,0 +1,75 @@
+use std::collections::HashMap;
+use std::hash::Hash;
+
+#[derive(Debug)]
+pub struct GraphErr {
+    message: String,
+}
+
+impl GraphErr {
+    pub fn new(s: &str) -> Self {
+        GraphErr {
+            message: s.to_string(),
+        }
+    }
+}
+
+// Map pointer based
+#[derive(Debug)]
+pub struct Graph<T, E, ID: Hash + Eq> {
+    data: HashMap<ID, (T, Vec<ID>)>,
+    edges: HashMap<ID, (E, ID, ID)>,
+}
+
+impl<T, E, ID: Clone + Hash + Eq> Graph<T, E, ID> {
+    pub fn new() -> Self {
+        Graph {
+            data: HashMap::new(),
+            edges: HashMap::new(),
+        }
+    }
+
+    pub fn add_node(&mut self, id: ID, data: T) {
+        // node does not have edges yet
+        self.data.insert(id, (data, Vec::new()));
+    }
+
+    pub fn add_edge(&mut self, ed_id: ID, from: ID, to: ID, edat: E) -> Result<(), GraphErr> {
+        if !self.data.contains_key(&from) {
+            return Err(GraphErr::new("'from' not in nodes"));
+        }
+        if let Some(ref mut dt) = self.data.get_mut(&to) {
+            self.edges.insert(ed_id.clone(), (edat, from.clone(), to));
+            dt.1.push(ed_id.clone());
+        } else {
+            return Err(GraphErr::new("'to' not in nodes"));
+        }
+
+        self.data.get_mut(&from).unwrap().1.push(ed_id);
+
+        Ok(())
+    }
+}
+
+fn main() -> Result<(), GraphErr> {
+    // cargo run --bin graph
+    let mut g = Graph::new();
+    for x in vec!['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'] {
+        g.add_node(x, ());
+    }
+
+    g.add_edge('a', 'H', 'D', 6)?;
+    g.add_edge('b', 'D', 'C', 18)?;
+    g.add_edge('c', 'C', 'B', 10)?;
+    g.add_edge('d', 'H', 'A', 7)?;
+    g.add_edge('e', 'A', 'C', 4)?;
+    g.add_edge('f', 'H', 'G', 5)?;
+    g.add_edge('g', 'G', 'A', 8)?;
+    g.add_edge('h', 'A', 'F', 3)?;
+    g.add_edge('i', 'F', 'E', 15)?;
+    g.add_edge('j', 'C', 'E', 12)?;
+
+    println!("Hello, graph {:?}", g);
+
+    Ok(())
+}
