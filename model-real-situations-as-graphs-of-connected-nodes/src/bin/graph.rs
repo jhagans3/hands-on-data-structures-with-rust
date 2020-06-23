9c80afce4509c036699322fa8e4cb4f0b7142e59
James Hagans <jhagans@acm.org>
2020-06-22 23:47:13 -0400

    Route Structure

--- model-real-situations-as-graphs-of-connected-nodes/src/bin/graph.rs
+++ model-real-situations-as-graphs-of-connected-nodes/src/bin/graph.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::hash::Hash;
+use std::rc::Rc;
 
 #[derive(Debug)]
 pub struct GraphErr {
@@ -11,6 +12,54 @@ impl GraphErr {
         GraphErr {
             message: s.to_string(),
         }
+    }
+}
+
+pub trait Weighted {
+    fn weight(&self) -> i32;
+}
+
+impl Weighted for i32 {
+    fn weight(&self) -> i32 {
+        *self
+    }
+}
+
+#[derive(Debug)]
+pub struct Route<ID> {
+    pos: ID,
+    path: Option<Rc<Route<ID>>>,
+    len: i32,
+}
+
+impl<ID: Eq> Route<ID> {
+    // wrap a new Route in an Rc
+    pub fn start_rc(pos: ID) -> Rc<Self> {
+        Rc::new(Route {
+            pos,
+            path: None,
+            len: 0,
+        })
+    }
+
+    // does the Rout contain a position
+    pub fn contains(&self, id: &ID) -> bool {
+        if self.pos == *id {
+            return true;
+        }
+        match self.path {
+            Some(ref p) => p.contains(id),
+            None => false,
+        }
+    }
+}
+
+impl<ID: fmt::Debug> fmt::Display for Route<ID> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if let Some(ref p) = self.path {
+            write!(f, "{}-{}-", p, self.len)?;
+        }
+        write!(f, "{:?}", self.pos)
     }
 }
 
